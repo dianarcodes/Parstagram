61fd7403f69c18e52bd98bb1c196df25f4c214fe
dianarcodes <94495024+dianarcodes@users.noreply.github.com>
2022-03-26 13:45:50 -0400

    Add files via upload

---
+++ FeedViewController.swift
@@ -0,0 +1,82 @@
+//
+//  FeedViewController.swift
+//  Instagram
+//
+//  Created by Diana Rivas on 3/26/22.
+//
+
+import UIKit
+import Parse
+import AlamofireImage
+
+class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var posts = [PFObject]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.delegate = self
+        tableView.dataSource = self
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        //get the query
+        let query = PFQuery(className:"Posts")
+        query.includeKey("author")
+        query.limit = 20
+        //store the data
+        query.findObjectsInBackground{ (posts, error) in
+            if posts != nil {
+                self.posts = posts!
+                self.tableView.reloadData() //reload the tableView
+            }
+        }
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+        
+        let post = posts[indexPath.row]
+        
+        let user = post["author"] as! PFUser
+        cell.usernameLabel.text = user.username
+        
+        cell.captionLabel.text = post["caption"] as? String
+        
+        //will load the image
+        let imageFile = post["image"] as! PFFileObject
+        let urlString = imageFile.url!
+        let url = URL(string: urlString)!
+        
+        //modification. Previously was: cell.photoView.af_setImage(withURL: url)
+        let data = try! Data(contentsOf: url)
+        cell.photoView.image = UIImage(data: data)
+        
+        return cell
+        
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
